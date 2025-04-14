c1f343ef1a9fef9a060a6fa47467d5cef09e4c82
Shubham Kumar Yadav <shubhamky1612@gmail.com>
2025-04-14 16:07:01 +0530

    update the chart while chage the amount

--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import TransactionFormSkeleton from "../components/skeleton/TransactionFormSkeleton";
 import { useParams } from "react-router-dom"
 import { useMutation, useQuery } from "@apollo/client";
-import { GET_TRANSACTION } from "../graphql/mutations/queries/transaction.query";
+import { GET_TRANSACTION, GET_TRANSACTION_STATISTICS } from "../graphql/mutations/queries/transaction.query";
 import { UPDATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
 import toast from "react-hot-toast";
 
@@ -17,7 +17,7 @@ const TransactionPage = () => {
 	
 
 const [updateTransaction,{loading: loadingUpdate}] = useMutation(UPDATE_TRANSACTION, {
-	refetchQueries: ["GetTransactions","GetTransactionStatistics"],
+	refetchQueries: [{ query: GET_TRANSACTION_STATISTICS }],
 });
 
 	const [formData, setFormData] = useState({
